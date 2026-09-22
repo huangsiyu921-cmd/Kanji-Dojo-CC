4b2639f97ef0ad685a7d01168e21763aa35c8005
Sensei <sensei@local>
2026-09-22 19:05:51 +0800

    build: 桌面版版本号 1.0.0 -> 1.0.1

--- buildSrc/src/main/kotlin/AppVersion.kt
+++ buildSrc/src/main/kotlin/AppVersion.kt
@@ -5,6 +5,6 @@ object AppVersion {
     const val versionName = "0.0.2alpha"
 
     // Requires 3 numbers, and MAJOR must be > 0 for the desktop DMG/MSI packaging
-    const val desktopAppVersion = "1.0.0"
+    const val desktopAppVersion = "1.0.1"
 
 }
